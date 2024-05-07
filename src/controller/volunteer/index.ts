e0f68eaee662720f136ac669c2760aa6c32ab05e
Mohini <mohini872601@gmail.com>
2024-05-07 14:00:21 +0530

    new

--- src/controller/volunteer/index.ts
+++ src/controller/volunteer/index.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import nodemailer from 'nodemailer'
 import yenv from "yenv";
 const env = yenv("env.yaml", { env: "development" });
 const UserSchema = require('../../model/volunteer/user');
@@ -95,4 +96,124 @@ export const loginUser = async (req: Request, res: Response) => {
   }
 };
 
-module.exports = { registerUser, loginUser };
+export const ChangePassword = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body; 
+    // Find user by email
+    const user = await UserSchema.findOne({ email });
+
+    // Check if user exists
+    if (!user) {
+      return res.status(404).json({
+        code: 404,
+        message: "User not found",
+        error: true,
+        status: false,
+      });
+    }
+
+    // Check password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        code: 401,
+        message: "Invalid password",
+        error: true,
+        status: false,
+      });
+    }
+
+    // User authenticated, generate JWT token
+    const token = jwt.sign({ userId: user._id },env.JWT_SECRET, { expiresIn: '1h' });
+
+    return res.status(200).json({
+      code: 200,
+      message: "Login successful",
+      token: token,
+      user,
+      error: false,
+      status: true,
+    });
+  } catch (err) {
+    console.error("Error logging in user:", err);
+    return res.status(500).json({
+      code: 500,
+      message: "Internal Server Error",
+      error: true,
+      status: false,
+    });
+  }
+};
+const sendEmail = async (to:any, subject:any, text:any) => {
+  try {
+      const transporter = nodemailer.createTransport({
+          host: env.MAIL_HOST,
+          port: env.MAIL_PORT,
+          secure: false,
+          auth: {
+                 user:env.MAIL_USERNAME,
+                 pass:env.MAIL_PASSWORD
+              }
+      });
+      const mailOptions = {
+          from:env.MAIL_USERNAME ,
+          to: `${to}`,
+          subject: subject,
+          text: " ",
+          html: "<p> click below, To Reset <a href='https://techwagger.com/reset-password/" + text + "'> click here,</a></p>"
+//
+      };
+       await transporter.sendMail(mailOptions);
+  } catch (error) {
+      console.error('Error sending email:', error);
+      throw new Error('Failed to send email');
+  }
+};
+const generateOTP = () => {
+  const otpLength = 10;
+  const digits = '0123456789';
+  let OTP = '';
+  for (let i = 0; i < otpLength; i++) {
+      OTP += digits[Math.floor(Math.random() * 10)];
+  }
+  return OTP;
+};
+export const forgetPassword = async (req: any, res: any) => {
+  try {
+      const { email } = req.body;
+      const OTP = await generateOTP();
+      //  const user = await Users.findOne({where:{ email }});
+      //  if (!user) {
+      //     return res.status(404).json({
+      //         status: 200,
+      //         error: false,
+      //         message: "User not found",
+      //         data: []
+      //       });
+      // }
+      const otp= OTP;
+     // const otpCreatedAt = new Date();
+     // await Users?.update({email}, {otp, otpCreatedAt});
+      await sendEmail(email,"Techwagger Password Reset",otp);
+      return res.status(200).json({
+          status: 200,
+          error: false,
+          message: "Link sent to  your mail, successfully.",
+          data: {
+                  email
+                },
+        });
+  } catch (error) {
+      return res.status(500).json({
+          status: 500,
+          error: true,
+          message: "Internal Server Error",
+          data: []
+        });
+}}
+
+
+
+
+
--- src/route/volunteer/index.ts
+++ src/route/volunteer/index.ts
@@ -1,6 +1,6 @@
 import express from "express"; 
  const routes = express.Router();
- import {loginUser, registerUser} from '../../controller/volunteer/index'
+ import {forgetPassword, loginUser, registerUser} from '../../controller/volunteer/index'
 import { imgupload } from "../../middleware/multer";
 import { requireSignIn } from "../../middleware";
 // create post 
@@ -10,6 +10,8 @@ routes.post("/upload",requireSignIn,imgupload.single("img"),(req:any,res:any)=>{
    console.log(req.file,"00000000000000");
     res.send({e:req.user,file:req.file})
 });
+
+routes.post("/email",forgetPassword);
 export default routes;
 
  
