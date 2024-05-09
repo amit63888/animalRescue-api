b0f75de722868f9b0bc71e0354f9fc0cc5ae473e
amit6388 <amitpoly2020@gmail.com>
2024-05-09 13:20:11 +0530

    text

--- src/route/volunteer/index.ts
+++ src/route/volunteer/index.ts
@@ -2,7 +2,7 @@
 import express from "express"; 
 const routes = express.Router();
 import { forgetPassword, loginUser, registerUser } from '../../controller/volunteer/index';
-import { uploadImgFile } from "../../utills/upload";
+import { uploadImgFile, validateFile } from "../../utills/upload";
 
 // create post 
 routes.post("/regiser", registerUser);
@@ -13,12 +13,27 @@ routes.post("/upload", async (req: any, res: any) => {
             return res.status(400).send('No files were uploaded.');
         } 
         const { file } = req.files;  
+        const validationError = await validateFile(file);
+        if (validationError) {
+            return res.status(404).json({
+                code: 404,
+                message: validationError,
+                error: true,
+                status: false,
+              });
+        }
         const currentDir ='./src/upload';  
         const fullPath = await uploadImgFile(currentDir, file);  
-        res.send({ message: 'File uploaded successfully', filePath: fullPath });
+        return res.status(200).json({
+            code: 200,
+            status: true,
+            data: fullPath,
+            message: 'File uploaded successfully',
+            error: false,
+          });
     } catch (error) {
         console.error('Error uploading file:', error);
-        res.status(500).send('Internal Server Error');
+        return res.status(500).send('Internal Server Error');
     }
 });
 
--- src/utills/upload.ts
+++ src/utills/upload.ts
@@ -12,3 +12,16 @@ export const uploadImgFile = (uploadPath: string, file: any) => {
     });
   });
 };
+ export const validateFile = (file: any) => {
+  // Define allowed file types
+  const allowedTypes = ['image/jpeg','image/png'];
+  // Define max file size (in bytes)
+  const maxSize = 5 * 1024 * 1024;  
+  if (!allowedTypes.includes(file.mimetype)) {
+      return 'Invalid file type. Only JPEG and PNG files are allowed.';
+  } 
+  if (file.size > maxSize) {
+      return 'File size exceeds the maximum allowed size of 5 MB.';
+  } 
+  return null;
+};
